f0f2a4ecd84f0f4d398b0cf079dfcf5e86ba7ebe
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-08 17:46:50 +0530

    Update Battery_Parameter_Handler.py

--- Battery_Parameter_Handler.py
+++ Battery_Parameter_Handler.py
@@ -1,5 +1,4 @@
 import random
-import numpy
 import report_and_alert
 
 battery_limits = {'TEMPERATURE': [0, 45], 'SOC': [20, 80], 'CHARGE_RATE': [0, 0.8]} 
@@ -12,29 +11,29 @@ def generating_random_number(min, max):
     return random.uniform(min, max)    
 
 def process_information(battery_parameters, alert_type):
-    check_parameters(battery_parameters)
-    check_alert(alert_type)    
-    for battery_parameter in battery_parameters:
-        parameter_values = generate_limits(battery_parameter.upper())      
-        publish_data(parameter_values, battery_parameter, alert_type.upper())
-    return True
+  check_parameters(battery_parameters)
+  check_alert(alert_type)    
+  for battery_parameter in battery_parameters:
+    parameter_values = generate_limits(battery_parameter.upper())
+    publish_data(parameter_values, battery_parameter, alert_type.upper())
+  return True
 
 def check_paramters(battery_parameters):
-    if len(battery_parameters)== 0: 
-        raise ValueError('Battery parameter is missing')
-    for item in battery_parameters:
-        if not item.upper() in battery_limits.keys():
-            return 'Battery paramenter is wrong'
+  if len(battery_parameters)== 0: 
+    return 'Battery parameter is missing'
+  for item in battery_parameters:
+    if not item.upper() in battery_limits.keys():
+      return 'Battery paramenter is wrong'
 
-def check_alert(alert_type):    
-    if len(alert_type) == 0:
-        return 'Alert type is missing'
-    elif not alert_type.upper() in report_and_alert.alert_Target_type.keys():
-        return 'Alert Type is Wrong'          
+def check_alert(alert_type): 
+  if len(alert_type) == 0:
+    return 'Alert Type is missing'
+  elif not alert_type.upper() in report_and_alert.alert_Target_type.keys():
+    return 'Alert Type is Wrong'          
             
 def generate_limits(battery_parameter):
-    min, max = battery_limits[battery_parameter]
-    return generating_random_number(min, max)
+  min, max = battery_limits[battery_parameter]
+  return generating_random_number(min, max)
  
 def publish_data(parameter_values,battery_parameter,alert_type):
-    return report_and_alert.check_and_alert(parameter_values, battery_parameter,alert_type)
+  return report_and_alert.check_and_alert(parameter_values, battery_parameter,alert_type)
