59bc51ae25561dbd1280fa822384a55ae6287a27
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-08 19:18:37 +0530

    Update bms_test.py

--- bms_test.py
+++ bms_test.py
@@ -13,10 +13,10 @@ class test_battery_current_ranges(unittest.TestCase):
     self.assertTrue(Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"IPHONE") == "Alert Type is Wrong")
     
   def test_wrong_batteryParam(self):
-    self.assertTrue(Battery_Parameter_Handler.process_information(["Volumne" ,"help", "Soc"],"Email") == "Battery parameter is wrong")         
+    self.assertTrue(Battery_Parameter_Handler.process_information(["Volumne" ,"help", "Soc"],"Email"))         
     
   def test_empty_alertType(self):
-    self.assertTrue(Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"") == "Alert Type is missing")
+    self.assertTrue(Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],""))
     
   def test_empty_batteryParam(self):
     self.assertTrue(Battery_Parameter_Handler.process_information([],"Email"))   
