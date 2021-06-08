6ee4ea2c3a60d16f1ef39c58d64332e8e8bbffb9
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-08 18:31:08 +0530

    Update bms_test.py

--- bms_test.py
+++ bms_test.py
@@ -10,16 +10,16 @@ class test_battery_current_ranges(unittest.TestCase):
     self.assertTrue(Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"Console"))
     
   def test_wrong_alertType(self):
-    self.assertTrue((Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"IPHONE")) == 'Alert Type is Wrong')
+    self.assertFalse((Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"IPHONE")) == 'Alert Type is Wrong')
     
   def test_wrong_batteryParam(self):
-    self.assertTrue((Battery_Parameter_Handler.process_information(["Volumne" ,"Temperature", "Soc"],"Email")) == 'Battery paramenter is wrong')         
+    self.assertFalse((Battery_Parameter_Handler.process_information(["Volumne" ,"Temperature", "Soc"],"Email")) == 'Battery paramenter is wrong')         
     
   def test_empty_alertType(self):
-    self.assertTrue((Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],""))== 'Alert Type is missing')
+    self.assertFalse((Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],""))== 'Alert Type is missing')
     
   def test_empty_batteryParam(self):
-    self.assertTrue((Battery_Parameter_Handler.process_information([],"Email")) == 'Battery paramenter is missing')   
+    self.assertFalse((Battery_Parameter_Handler.process_information([],"Email")) == 'Battery paramenter is missing')   
     
 if __name__ == '__main__':
   unittest.main()
