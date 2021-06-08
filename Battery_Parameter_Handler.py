c29834473ae6bf509302e97ebc164444d247c443
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-08 17:28:09 +0530

    Update and rename Battery_Parameter_Info.py to Battery_Parameter_Handler.py

---
+++ Battery_Parameter_Handler.py
@@ -0,0 +1,40 @@
+import random
+import numpy
+import report_and_alert
+
+battery_limits = {'TEMPERATURE': [0, 45], 'SOC': [20, 80], 'CHARGE_RATE': [0, 0.8]} 
+
+def generating_random_number(min, max):
+  #Generates random numbers between min and max
+  if isinstance((min+max) , int):
+    return random.randint(min, max)
+  elif isinstance((min+max) , float):
+    return random.uniform(min, max)    
+
+def process_information(battery_parameters, alert_type):
+    check_parameters(battery_parameters)
+    check_alert(alert_type)    
+    for battery_parameter in battery_parameters:
+        parameter_values = generate_limits(battery_parameter.upper())      
+        publish_data(parameter_values, battery_parameter, alert_type.upper())
+    return True
+
+def check_paramters(battery_parameters):
+    if len(battery_parameters)== 0: 
+        raise ValueError('Battery parameter is missing')
+    for item in battery_parameters:
+        if not item.upper() in battery_limits.keys():
+            raise KeyError('Battery paramenter is wrong')
+
+def check_alert(alert_type):    
+    if len(alert_type) == 0:
+        raise ValueError('Alert type is missing')
+    elif not alert_type.upper() in report_and_alert.alert_Target_type.keys():
+        raise KeyError("Alert Type is Wrong")          
+            
+def generate_limits(battery_parameter):
+    min, max = battery_limits[battery_parameter]
+    return generating_random_number(min, max)
+ 
+def publish_data(parameter_values,battery_parameter,alert_type):
+    return report_and_alert.check_and_alert(parameter_values, battery_parameter,alert_type)
