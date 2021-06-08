3ed6a5d6ea55fae6fe27e7ffa9e44f10ca039e80
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-08 19:08:54 +0530

    Update bms_test.py

--- bms_test.py
+++ bms_test.py
@@ -19,7 +19,7 @@ class test_battery_current_ranges(unittest.TestCase):
     self.assertTrue(Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"") == "Alert Type is missing")
     
   def test_empty_batteryParam(self):
-    self.assertEqual(Battery_Parameter_Handler.process_information([],"Email") , "Battery parameter is missing")   
+    self.assertEqual(Battery_Parameter_Handler.process_information([],"Email") , True)   
     
 if __name__ == '__main__':
   unittest.main()
