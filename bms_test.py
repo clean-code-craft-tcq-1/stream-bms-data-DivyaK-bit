1cced1c8a4ca8d06d5f7165a17d56151700282b6
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-09 09:43:52 +0530

    Update bms_test.py

--- bms_test.py
+++ bms_test.py
@@ -12,22 +12,22 @@ class test_battery_current_ranges(unittest.TestCase):
    def test_wrong_alertType(self):
       with self.assertRaises(KeyError) as context:
             bms_data_handler.process_vitals(["Charge_rate" ,"Temperature", "Soc"],"PDF")
-      self.assertTrue('Supplied AlertType is wrong' in str(context.exception))
+      self.assertTrue('Alert Type is wrong' in str(context.exception))
       
   def test_wrong_batteryParam(self):
       with self.assertRaises(KeyError) as context:
             Battery_Parameter_Handler.process_information(["Voltage" ,"Temperature", "Soc"],"Email")
-      self.assertTrue('Supplied battery paramenter is wrong' in str(context.exception))         
+      self.assertTrue('Battery paramenter is wrong' in str(context.exception))         
       
   def test_empty_alertType(self):
       with self.assertRaises(ValueError) as context:
             Battery_Parameter_Handler.process_information(["Charge_rate" ,"Temperature", "Soc"],"")
-      self.assertTrue('Alerttype value is missing' in str(context.exception))
+      self.assertTrue('Alert Type is missing' in str(context.exception))
       
   def test_empty_batteryParam(self):
       with self.assertRaises(ValueError) as context:
             Battery_Parameter_Handler.process_information([],"Email")
-      self.assertTrue('Battery parameter value is missing' in str(context.exception))
+      self.assertTrue('Battery parameter is missing' in str(context.exception))
     
 if __name__ == '__main__':
   unittest.main()
